f69f3292466b3e1e74cb9b201000a3aa1523337b
Chris Balla <ballachris13@gmail.com>
2024-06-27 13:01:16 +0200

    creating entities/Models

---
+++ Model/Logiciel.cs
@@ -0,0 +1,37 @@
+﻿
+namespace JobOverview.Model
+{
+   public class Logiciel
+   {
+      public int CodeLogiciel { get; set; }
+      public string Nom {  get; set; } = string.Empty;
+   }
+
+   public class Module
+   {
+      public int CodeModule { get; set; }
+      public string Nom { get; set; } = string.Empty ;
+   }
+
+   public class Filière
+   {
+      public int CodeFilière { get; set; }
+      public string Nom { get; set; } = string.Empty;
+   }
+
+   public class Release
+   {
+      public int NumeroRelease { get; set; }
+      public DateTime DatePublication { get; set; }
+      public int NumeroVersion { get; set; }
+   }
+
+   public class Version
+   {
+      public int NumeroVersion { get; set; }
+      public int Millesime { get; set; }
+      public DateTime DateOuverture { get; set; }
+      public DateTime DateSortiePrevue { get; set; }
+      public DateTime DateSortieReelle { get; set; }
+   }
+}
