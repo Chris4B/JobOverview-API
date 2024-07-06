afac21cd44119b4c3df1e5d7715eec1a695c9849
Chris Balla <ballachris13@gmail.com>
2024-07-06 08:46:57 +0200

    adding relations between entities

--- Data/JobOverviewContext.cs
+++ Data/JobOverviewContext.cs
@@ -8,53 +8,75 @@ namespace JobOverview.Data
    {
       public JobOverviewContext(DbContextOptions<JobOverviewContext> options): base(options) { }
 
-      public DbSet<Logiciel> Logiciels { get; set; }
-      public DbSet<Module> Modules { get; set; }
-      public DbSet<Filiere> Filieres { get; set; }
+      public DbSet<Logiciel> Logiciel { get; set; }
+      public DbSet<Module> Module { get; set; }
+      public DbSet<Filiere> Filiere { get; set; }
 
-      public DbSet<Release> Releases { get; set; }
+      public DbSet<Release> Release { get; set; }
 
-      public DbSet<Version> Versions { get; set; }
+      public DbSet<Version> Version { get; set; }
 
 
       protected override void OnModelCreating(ModelBuilder modelBuilder)
       {
          modelBuilder.Entity<Module>(entity =>
          {
+            entity.ToTable("Modules");
+            //entity.HasKey(entity => new { entity.CodeLogiciel, entity.CodeModule });
             entity.HasKey(entity => entity.CodeModule).HasName("PrimaryKey_CodeModule");
             entity.Property(entity => entity.CodeModule).HasMaxLength(20).IsUnicode(false);
             entity.Property(entity => entity.Nom).HasMaxLength(60);
+            entity.Property(entity => entity.CodeLogicielParent).HasMaxLength(20).IsUnicode(false);
+            entity.Property(entity => entity.CodeModuleParent).HasMaxLength(20).IsUnicode(false);
+            entity.Property(entity => entity.CodeLogiciel).HasMaxLength(20).IsUnicode(false); 
+
+            //entity.HasOne<Logiciel>().WithMany().HasForeignKey(d => d.CodeModule).OnDelete(DeleteBehavior.NoAction);
             
          });
 
          modelBuilder.Entity<Logiciel>(entity =>
          {
+            entity.ToTable("Logiciels");
             entity.HasKey(entity => entity.CodeLogiciel).HasName("PrimaryKey_CodeLogiciel");
             entity.Property(entity => entity.CodeLogiciel).HasMaxLength(20).IsUnicode(false);
             entity.Property(entity => entity.Nom).HasMaxLength(60);
+            entity.Property(entity => entity.CodeFilière).HasMaxLength(20).IsUnicode(false); 
+
+            entity.HasOne<Filiere>().WithMany().HasForeignKey(d => d.CodeFilière).OnDelete(DeleteBehavior.NoAction);
 
          });
 
          modelBuilder.Entity<Filiere>(entity =>
          {
+            entity.ToTable("Filières");
             entity.HasKey(entity => entity.CodeFilière).HasName("PrimaryKey_CodeFiliere");
             entity.Property(entity => entity.CodeFilière).HasMaxLength(20);
             entity.Property(entity => entity.Nom).HasMaxLength(60);
+            
 
          });
 
          modelBuilder.Entity<Release>(entity =>
          {
+            entity.ToTable("Releases");
+
+            //entity.HasKey(entity => new { entity.CodeLogiciel, entity.NumeroRelease, entity.NumeroVersion });
             entity.HasKey(entity => entity.NumeroRelease).HasName("PrimaryKey_NumeroRelease");
+
             entity.Property(entity => entity.NumeroRelease).HasColumnType("smallint");
             entity.Property(entity => entity.NumeroVersion).HasColumnType("real");
             entity.Property(entity => entity.DatePublication).HasColumnType("DateTime");
+            entity.Property(entity => entity.CodeLogiciel).HasMaxLength(20);
 
          });
 
          modelBuilder.Entity<Version>(entity =>
          {
-            entity.HasKey(entity => entity.NumeroVersion).HasName("primaryKey_NumeroVersion");
+            entity.ToTable("Versions");
+
+            //entity.HasKey(entity => new { entity.CodeLogiciel, entity.NumeroVersion });
+            entity.HasKey(entity => entity.NumeroVersion).HasName("PrimaryKey_NumeroVersion");
+
             entity.Property(entity => entity.NumeroVersion).HasColumnType("real");
             entity.Property(entity => entity.Millesime).HasColumnType("smallint");
             entity.Property(entity => entity.DateOuverture).HasColumnType("datetime");
--- Model/Logiciel.cs
+++ Model/Logiciel.cs
@@ -1,37 +1,54 @@
 ﻿
 namespace JobOverview.Model
 {
+   /// <summary>
+   /// classe représentant un logicile 
+   /// </summary>
    public class Logiciel
    {
-      public int CodeLogiciel { get; set; }
-      public string Nom {  get; set; } = string.Empty;
+      public string CodeLogiciel { get; set; } = "";
+      public string Nom {  get; set; } = "";
+
+      // ajout de la propriété qui servira de clé étrangère
+      public string CodeFilière { get; set; } = "";
+
+      
    }
 
    public class Module
    {
-      public int CodeModule { get; set; }
+      public string CodeModule { get; set; } = string.Empty;
       public string Nom { get; set; } = string.Empty ;
+
+      public string? CodeModuleParent { get; set; }
+      public string? CodeLogiciel { get; set; }
+
+      public string? CodeLogicielParent { get; set; }
    }
 
    public class Filiere
    {
-      public int CodeFilière { get; set; }
+      public string CodeFilière { get; set; } = "";
       public string Nom { get; set; } = string.Empty;
    }
 
    public class Release
    {
-      public int NumeroRelease { get; set; }
+      public short NumeroRelease { get; set; }
       public DateTime DatePublication { get; set; }
-      public int NumeroVersion { get; set; }
+      
+      public float NumeroVersion { get; set; }
+      public string CodeLogiciel { get; set; } = "";
    }
 
    public class Version
    {
-      public int NumeroVersion { get; set; }
-      public int Millesime { get; set; }
+      public float NumeroVersion { get; set; }
+      public short Millesime { get; set; }
       public DateTime DateOuverture { get; set; }
       public DateTime DateSortiePrevue { get; set; }
-      public DateTime DateSortieReelle { get; set; }
+      public DateTime? DateSortieReelle { get; set; }
+
+      public string CodeLogiciel { get; set; } = "";
    }
 }
